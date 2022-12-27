16ee7c51bd2e92095131206ad0b5ec2688ed2cf9
Patryk Szabat <szabat.patryk@gmail.com>
2022-12-27 18:08:50 +0100

    css reset + images

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,16 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false,
   theme: {
+    colors: {
+      red: {
+        100: 'hsl(0, 0%, 100%)',
+        200: 'hsl(0, 80%, 86%)',
+        300: 'hsl(0, 74%, 74%)',
+        400: 'hsl(0, 36%, 70%)',
+        700: 'hsl(0, 93%, 68%)',
+        900: 'hsl(0, 6%, 24%)'
+      }
+    },
     extend: {},
   },
   variants: {
