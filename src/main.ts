098e55f319d100937a30fe71955f25bf7a56010e
Patryk Szabat <szabat.patryk@gmail.com>
2022-12-28 13:14:51 +0100

    ts + css + html

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
-export const test = []
+export const input = document.getElementById('input') as HTMLInputElement
+export const button = document.getElementById('btn') as HTMLButtonElement
+const error = document.getElementById('error') as HTMLDivElement
+
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g as RegExp
+    const inputValue = input.value.length > 0 as boolean
+    const verifieEmail = input.value.match(regex) as RegExpMatchArray | null
+    if (!inputValue) {
+        error.innerHTML = `Email field is empty`
+    }
+    if (inputValue && !verifieEmail) {
+        error.innerHTML = `Please provide a valid email address`
+    }
+    if (verifieEmail) {
+        error.innerHTML = ``
+    }
+})
